8c6e7ed97a8fcf0bfb2a902d255f3f25152bde25
Timofei Beresnjev <bertimblch@gmail.com>
2023-09-10 20:20:23 +0300

    Initial commit

---
+++ src/test/java/ee/bertim/SpringGameApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.bertim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
